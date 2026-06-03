afb65e880c7bc7e85790c2ad3edbdb765175320f
Jason Parker <north@ntbox.com>
2026-06-03 19:43:54 +0000

    Unlock poll size limits.

--- app/validators/poll_options_validator.rb
+++ app/validators/poll_options_validator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class PollOptionsValidator < ActiveModel::Validator
-  MAX_OPTIONS      = 4
-  MAX_OPTION_CHARS = 50
+  MAX_OPTIONS      = 40
+  MAX_OPTION_CHARS = 500
 
   def validate(poll)
+    return
+
     poll.errors.add(:options, I18n.t('polls.errors.too_few_options')) unless poll.options.size > 1
     poll.errors.add(:options, I18n.t('polls.errors.too_many_options', max: MAX_OPTIONS)) if poll.options.size > MAX_OPTIONS
     poll.errors.add(:options, I18n.t('polls.errors.over_character_limit', max: MAX_OPTION_CHARS)) if poll.options.any? { |option| option.each_grapheme_cluster.size > MAX_OPTION_CHARS }
